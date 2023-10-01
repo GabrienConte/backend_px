5f5ba6d178c8245b21b8a7383476088dd8a10dfc
GabrienConte <gabrielconte0@gmail.com>
2023-10-01 17:56:51 -0300

    first commit. pastas e dependencias criadas

---
+++ src/main/java/br/ufsm/backend_px/BackendPxApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.backend_px;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/backend_px/BackendPxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.backend_px;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
