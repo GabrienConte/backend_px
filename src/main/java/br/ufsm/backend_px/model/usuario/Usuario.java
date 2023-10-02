00bbed67588785b8e1858abc5925aa329b2dcc6b
GabrienConte <gabrielconte0@gmail.com>
2023-10-02 19:01:33 -0300

    Implementação do sistema de segurança do projeto e crição e implementação do Model, Service, Repository e Controller do Usuario

--- src/main/java/br/ufsm/backend_px/model/usuario/Usuario.java
+++ src/main/java/br/ufsm/backend_px/model/usuario/Usuario.java
@@ -1,4 +1,34 @@
 package br.ufsm.backend_px.model.usuario;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nome;
+    @NotNull
+    @Email
+    private String login;
+    @NotNull
+    private String senha;
+    private String permissao;
+    private String descricao;
+    private String facebook;
+    private String instagram;
+    private String linkedin;
+    private String github;
 }
