67eeef1d4dff189cb6eb3aa576ca76aa5a9025c2
Augusto <augusto.p.h@hotmail.com>
2023-10-02 11:13:47 -0300

    JPA - Linguagem

--- src/main/java/br/ufsm/backend_px/model/linguagem/Linguagem.java
+++ src/main/java/br/ufsm/backend_px/model/linguagem/Linguagem.java
@@ -1,6 +1,25 @@
 package br.ufsm.backend_px.model.linguagem;
 
+/*
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "linguagem")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Linguagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
 }
+*/
