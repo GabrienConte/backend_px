70905524c05fe86b7a5057f4133680e3563fc213
Augusto <augusto.p.h@hotmail.com>
2023-10-02 21:01:53 -0300

    Linguagem

---
+++ src/main/java/br/ufsm/backend_px/model/linguagem/LinguagemRepository.java
@@ -0,0 +1,6 @@
+package br.ufsm.backend_px.model.linguagem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinguagemRepository extends JpaRepository<Linguagem, Long> {
+}
